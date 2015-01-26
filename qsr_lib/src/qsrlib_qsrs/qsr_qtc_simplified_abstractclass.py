d56ffb441ca79780a822c1f7a1666446891c975b
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-01-26 12:30:11 +0000

    Added correct validation function

--- qsr_lib/src/qsrlib_qsrs/qsr_qtc_simplified_abstractclass.py
+++ qsr_lib/src/qsrlib_qsrs/qsr_qtc_simplified_abstractclass.py
@@ -65,7 +65,7 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
             return None, None
         return ret_str, ret_int
 
-    def validate_qtc_sequence(self, qtc):
+    def _validate_qtc_sequence(self, qtc):
         """Removes illegal state transition by inserting necessary intermediate states
 
         :param qtc: a numpy array of the qtc state chain. One qtc state per row
@@ -75,34 +75,54 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
         if self.qtc_type == "b":
             qtc = qtc[:,0:2]
 
-        newqtc = qtc[0].copy()
-        j = 1
-        for i in xrange(1, len(qtc)):
-            checksum = np.nonzero(qtc[i-1]+qtc[i] == 0)
-            intermediate = qtc[i].copy()
-            if checksum[0].size > 0:
-                for idx in checksum[0]:
-                    if np.absolute(qtc[i-1][idx]) + np.absolute(qtc[i][idx]) > 0:
-                        intermediate[idx] = 0
-                if np.any(intermediate != qtc[i]):
-                    newqtc = np.append(newqtc, intermediate)
-                    j += 1
-            newqtc = np.append(newqtc, qtc[i])
-            j += 1
+        if len(qtc.shape) == 1:
+            return qtc  # Only one state in chain.
 
-        return newqtc.reshape(-1, 2) if self.qtc_type == "b" else newqtc.reshape(-1,4)
+        legal_qtc = np.array([qtc[0,:]])
 
-    def create_qtc_representation(self, pos_k, pos_l, quantisation_factor=0):
-        """Creating the QTCC representation for the given data. Uses the 
-        double cross to determine to which side of the lines the points are 
+        for i in xrange(1, qtc.shape[0]):
+            insert = np.array(qtc[i,:].copy())
+            ###################################################################
+            # self transition = 0, transition form - to + and vice versa = 2
+            # transitions from - to 0 or + to 0 and vice versa = 1
+            insert[np.abs(qtc[i-1,:]-insert)>1] = 0
+
+            ###################################################################
+            # find invalid transitions according to CND:
+            # 1,2: -000 <> 0-00 | +000 <> 0+00 | -000 <> 0+00 | +000 <> 0-00
+            # 1,3: -000 <> 00-0 | +000 <> 00+0 | -000 <> 00+0 | +000 <> 00-0
+            # 1,4: -000 <> 000- | +000 <> 000+ | -000 <> 000+ | +000 <> 000-
+            # 2,3: 0-00 <> 00-0 | 0+00 <> 00+0 | 0-00 <> 00+0 | 0+00 <> 00-0
+            # 2,4: 0-00 <> 000- | 0+00 <> 000+ | 0-00 <> 000+ | 0+00 <> 000-
+            # 3,4: 00-0 <> 000- | 00+0 <> 000+ | 00-0 <> 000+ | 00+0 <> 000-
+            for j1 in xrange(0, len(qtc[i,:])-1):
+                for j2 in xrange(j1+1, len(insert)):
+                    if np.sum(np.abs(qtc[i-1,[j1,j2]])) == 1 \
+                    and np.sum(np.abs(insert[[j1,j2]])) == 1:
+                        if np.nanmax(np.abs(qtc[i-1,[j1,j2]] - insert[[j1,j2]])) > 0 \
+                        and np.sum(qtc[i-1,[j1,j2]] - insert[[j1,j2]]) != 1:
+                            insert[[j1,j2]] = 0
+
+            #print insert
+
+            if not np.array_equal(insert[np.logical_not(np.isnan(insert))], qtc[i, np.logical_not(np.isnan(insert))]):
+                legal_qtc = np.append(legal_qtc, [insert], axis=0)
+
+            legal_qtc = np.append(legal_qtc, [qtc[i,:]], axis=0)
+
+        return legal_qtc
+
+    def _create_qtc_representation(self, pos_k, pos_l, quantisation_factor=0):
+        """Creating the QTCC representation for the given data. Uses the
+        double cross to determine to which side of the lines the points are
         moving.
-        
+
         :param pos_k: An array of positions for agent k, exactly 2 entries of x,y positions
         :param pos_l: An array of positions for agent l, exactly 2 entries of x,y positions
         :param quantisation_factor: The minimum distance the points have to diverge from either line to be regrded a non-0-state
-        
+
         :return: The QTCC 4-tuple (q1,q2,q4,q5) for the movement of the two agents: [k[0],l[0],k[1],l[1]]
-        
+
         """
         #print "######################################################"
         pos_k = np.array(pos_k).reshape(-1, 2)
@@ -209,19 +229,19 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
         the side the point moved to (depending on the orientation of the line)
         and how far away it moved. The latter is used for the quantisation_factor
         and to create 0-states from noisy data.
-        Example: 
+        Example:
             - QTCB: uses the orthogonal line going through the point to compute the distance constraint
             - QTCC: uses the connecting line to determine the side constraint
-            
+
         :param pos: The position of the agent, point in space
         :param line: The line to use as reference for the movement
         :param quantisation_factor=0: Minimum distance a point has to move to be considered a non-0-state. same unit of mesuarment in which the points are described
-        :param constraint="": Set to "side" if checking for the side constraint. 
+        :param constraint="": Set to "side" if checking for the side constraint.
         The result for the side has to be inverted.
-        
+
         :return: The qtc symbol for this movement
 
-        """        
+        """
 
         x0 = pos[-1,0]
         y0 = pos[-1,1]
@@ -256,8 +276,8 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
             "x, y: the xy-coords of the agents\n" \
             "quantisation_factor: the minimum distance the agents must diverge from the double cross between two timesteps to be counted as movement. Must be in the same unit as the x,y coordinates.\n"\
             "validate: True|False validates the QTC sequence to not have illegal transitions. This inserts necessary transitional steps and messes with the timesteps."
-    
-    
+
+
     def custom_checks(self, input_data):
         """Write your own custom checks on top of the default ones
 
@@ -313,16 +333,16 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
                      input_data.trace[t0].objects[o2_name].y,
                      input_data.trace[t1].objects[o2_name].x,
                      input_data.trace[t1].objects[o2_name].y]
-                qtc_sequence = np.append(qtc_sequence, self.create_qtc_representation(
-                    k, 
-                    l, 
+                qtc_sequence = np.append(qtc_sequence, self._create_qtc_representation(
+                    k,
+                    l,
                     input_data.trace[t0].objects[o1_name].kwargs["quantisation_factor"]
                 )).reshape(-1,4)
-                
+
             except KeyError:
                 ret.add_empty_world_qsr_state(timestamp)
         if input_data.trace[0].objects[o1_name].kwargs["validate"]:
-            qtc_sequence = self.validate_qtc_sequence(qtc_sequence)
+            qtc_sequence = self._validate_qtc_sequence(qtc_sequence)
         for idx, qtc in enumerate(qtc_sequence):
             qtc_str = self.qtc_to_string((qtc))
             qsr = QSR(
@@ -331,18 +351,18 @@ class QSR_QTC_Simplified_Abstractclass(QSR_Abstractclass):
                 qsr=qtc_str
             )
             ret.add_qsr(qsr, idx+1)
-            
+
         return ret
-        
+
     @abstractmethod
     def qtc_to_string(self, qtc):
-        """Overwrite this for the different QTC veriants to select only the parts
+        """Overwrite this for the different QTC variants to select only the parts
         from the QTCC tuple that you would like to return.
         Example for QTCB: return str(qtc[0]) + "," + str(qtc[1])
-        
+
         :param qtc: The full QTCC tuple [q1,q2,q4,q5]
-        
-        :return: The part of the tuple you would to have as a result converted 
+
+        :return: The part of the tuple you would to have as a result converted
         to a comma separated string
         """
         return ""
