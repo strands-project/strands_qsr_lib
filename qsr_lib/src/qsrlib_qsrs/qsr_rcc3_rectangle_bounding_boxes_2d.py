53f8c339655bdc7e2d63e2e2ac072689b098fdc0
Yiannis Gatsoulis <y.gatsoulis@leeds.ac.uk>
2015-04-13 11:08:12 +0100

    changed to sorter code rcc3 custom checks for qsrs_for (same to arg_distance corrected one)

--- qsr_lib/src/qsrlib_qsrs/qsr_rcc3_rectangle_bounding_boxes_2d.py
+++ qsr_lib/src/qsrlib_qsrs/qsr_rcc3_rectangle_bounding_boxes_2d.py
@@ -43,15 +43,9 @@ class QSR_RCC3_Rectangle_Bounding_Boxes_2D(QSR_Abstractclass):
         :return: qsrs_for, error_found
         """
         for p in list(qsrs_for):
-            if type(p) is tuple:
-                if len(p) > 2:
-                    qsrs_for.remove(p)
-                    error_found = True
-            elif type(p) is str:
+            if (type(p) is not tuple) and (type(p) is not list) and (len(p) != 2):
                 qsrs_for.remove(p)
                 error_found = True
-            else:
-                raise ValueError("rcc3: qsrs_for must contain tuples of two objects")
         return qsrs_for, error_found
 
     def make(self, *args, **kwargs):
