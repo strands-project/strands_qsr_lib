ace45e637851fde399833827ff3dda25efe70191
Peter Lightbody <pet1330@gmail.com>
2015-08-11 17:55:50 +0100

    quantisation factors for RCC RCC2, RCC3, RCC8

--- qsr_lib/src/qsrlib_qsrs/qsr_rcc8_rectangle_bounding_boxes_2d.py
+++ qsr_lib/src/qsrlib_qsrs/qsr_rcc8_rectangle_bounding_boxes_2d.py
@@ -90,7 +90,6 @@ class QSR_RCC8_Rectangle_Bounding_Boxes_2D(QSR_Abstractclass):
                     ret.add_empty_world_qsr_state(timestamp)
         return ret
 
-    # custom functions follow
     def __return_all_possible_combinations(self, objects_names):
         if len(objects_names) < 2:
             return []
@@ -101,10 +100,11 @@ class QSR_RCC8_Rectangle_Bounding_Boxes_2D(QSR_Abstractclass):
                     ret.append((i, j))
         return ret
 
-    def __compute_qsr(self, bb1, bb2):
+    def __compute_qsr(self, bb1, bb2, q=0.0):
         """Return symmetrical RCC8 relation
             :param bb1: diagonal points coordinates of first bounding box (x1, y1, x2, y2)
             :param bb2: diagonal points coordinates of second bounding box (x1, y1, x2, y2)
+            :param q: quantisation factor for all objects
             :return: an RCC8 relation from the following:
                 'dc'     bb1 is disconnected from bb2
                 'ec'     bb1 is externally connected with bb2
@@ -122,68 +122,54 @@ class QSR_RCC8_Rectangle_Bounding_Boxes_2D(QSR_Abstractclass):
                  |            b|         |            d|
                  +-------------+         +-------------+
         """
-        
-        # Object 1 Top Left X
-        ax = bb1[0]
-        # Object 1 Top Left Y
-        ay = bb1[1]
-        # Object 2 Top Left X
-        cx = bb2[0]
-        # Object 2 Top Left X
-        cy = bb2[1]
-        # Object 1 Bottom Right X
-        bx = bb1[2]
-        # Object 1 Bottom Right Y
-        by = bb1[3]
-        # Object 2 Bottom Right X
-        dx = bb2[2]
-        # Object 2 Bottom Right Y
-        dy = bb2[3]
-    
+
         # CALCULATE EQ
         # Is object1 equal to object2
         if(bb1 == bb2):
             return "eq"
-    
+
+        ax, ay, bx, by = bb1
+        cx, cy, dx, dy = bb2
+
         # Are objects disconnected?
         # Cond1. If A's left edge is to the right of the B's right edge, - then A is Totally to right Of B
         # Cond2. If A's right edge is to the left of the B's left edge, - then A is Totally to left Of B
         # Cond3. If A's top edge is below B's bottom edge, - then A is Totally below B
         # Cond4. If A's bottom edge is above B's top edge, - then A is Totally above B
         
-        #    Cond1        Cond2        Cond3        Cond4
-        if (ax > dx) or (bx < cx) or (ay > dy) or (by < cy):
+        #    Cond1           Cond2          Cond3         Cond4
+        if (ax-q > dx+q) or (bx+q < cx-q) or (ay-q > dy+q) or (by+q < cy-q):
             return "dc"
-    
-        # Is one object inside the other
+
+        # Is one object inside the other ()
         BinsideA = (ax <= cx) and (ay <= cy) and (bx >= dx) and (by >= dy)
         AinsideB = (ax >= cx) and (ay >= cy) and (bx <= dx) and (by <= dy)
-    
+
         # Do objects share an X or Y (but are not necessarily touching)
-        sameX = (ax == cx or ax == dx or bx == cx or bx == dx)
-        sameY = (ay == cy or ay == dy or by == cy or by == dy)
-    
+        sameX = (abs(ax - cx)<=q) or (abs(ax - dx)<=q) or (abs(bx - cx)<=q) or (abs(bx - dx)<=q)
+        sameY = (abs(ay - cy)<=q) or (abs(ay - dy)<=q) or (abs(by - cy)<=q) or (abs(by - dy)<=q)
+        
         if AinsideB and (sameX or sameY):
             return "tpp"
-    
+
         if BinsideA and (sameX or sameY):
             return "tppi"
-    
+
         if AinsideB:
             return "ntpp"
-    
+
         if BinsideA:
             return "ntppi"
-    
+
         # Are objects touching?
         # Cond1. If A's left edge is equal to B's right edge, - then A is to the right of B and touching
         # Cond2. If A's right edge is qual to B's left edge, - then A is to the left of B and touching
         # Cond3. If A's top edge equal to B's bottom edge, - then A is below B and touching
         # Cond4. If A's bottom edge equal to B's top edge, - then A is above B and touching
-    
+
         #    Cond1        Cond2        Cond3        Cond4
-        if (ax == dx) or (bx == cx) or (ay == dy) or (by == cy):
+        if ((ax > dx+q) or (bx < cx-q) or (ay > dy+q) or (by < cy-q)and (sameX or sameY)):
             return "ec"
-    
+
         # If none of the other conditions are met, the objects must be parially overlapping
         return "po"
