eb34a5d3349e58042e7fe1c505f5f76fd51f6aa5
Yiannis Gatsoulis <y.gatsoulis@leeds.ac.uk>
2015-09-17 13:19:06 +0100

    added rcc5 for completeness

--- qsr_lib/src/qsrlib_qsrs/__init__.py
+++ qsr_lib/src/qsrlib_qsrs/__init__.py
@@ -1,5 +1,6 @@
 from qsr_rcc2_rectangle_bounding_boxes_2d import QSR_RCC2_Rectangle_Bounding_Boxes_2D
 from qsr_rcc3_rectangle_bounding_boxes_2d import QSR_RCC3_Rectangle_Bounding_Boxes_2D
+from qsr_rcc5_rectangle_bounding_boxes_2d import QSR_RCC5_Rectangle_Bounding_Boxes_2D
 from qsr_rcc8_rectangle_bounding_boxes_2d import QSR_RCC8_Rectangle_Bounding_Boxes_2D
 from qsr_cardinal_direction_bounding_boxes_centroid_2d import QSR_Cardinal_Direction_Bounding_Boxes_Centroid_2D
 from qsr_qtc_b_simplified import QSR_QTC_B_Simplified
@@ -13,6 +14,7 @@ from qsr_new_mwe import QSR_MWE
 # register new qsrs by class name below
 qsrs_registry = (QSR_RCC2_Rectangle_Bounding_Boxes_2D,
                  QSR_RCC3_Rectangle_Bounding_Boxes_2D,
+                 QSR_RCC5_Rectangle_Bounding_Boxes_2D,
                  QSR_RCC8_Rectangle_Bounding_Boxes_2D,
                  QSR_Cardinal_Direction_Bounding_Boxes_Centroid_2D,
                  QSR_QTC_B_Simplified,
